1b7b6c90608f73ace024e4408a959fcda198b6c6
Gerson Mayamba <gerson.maiamba@drcash.com.br>
2024-07-03 09:21:59 -0300

    create a gateway to test a cdk deloy pipeline

--- src/CdkPipelineDemo/CdkPipelineDemoStack.cs
+++ src/CdkPipelineDemo/CdkPipelineDemoStack.cs
@@ -1,4 +1,5 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.APIGateway;
 using Constructs;
 
 namespace CdkPipelineDemo
@@ -8,6 +9,22 @@ namespace CdkPipelineDemo
         internal CdkPipelineDemoStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             // The code that defines your stack goes here
+
+            var restAPI = new LambdaRestApi(this, "Teste-Api-Gateway-cdk", new LambdaRestApiProps
+            {
+                Proxy = true,        
+                EndpointTypes = new[] { EndpointType.REGIONAL },
+                RestApiName = "Teste-Api-Gateway-cdk",
+                Description= "Teste de API Gateway com CDK pepiline",
+                DeployOptions = new StageOptions
+                {
+                    StageName = "dev",
+                    Description = "Teste de API Gateway com CDK pepiline",
+                    TracingEnabled = false
+                }           
+            });
+
+            _ = new CfnOutput(this, "apigwtarn", new CfnOutputProps { Value = restAPI.ArnForExecuteApi() });
         }
     }
 }
