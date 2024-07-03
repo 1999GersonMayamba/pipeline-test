8fb837ee5d8e542aa1545d5a45b4e5005d1b35fb
Gerson Mayamba <gerson.maiamba@drcash.com.br>
2024-07-03 16:57:04 -0300

    teste

--- src/CdkPipelineDemo/Program.cs
+++ src/CdkPipelineDemo/Program.cs
@@ -12,11 +12,15 @@ namespace CdkPipelineDemo
             var app = new App();
             new CdkPipelineDemoStack(app, "CdkPipelineDemoStack", new StackProps
             {
-
                 Env = new Amazon.CDK.Environment
                 {
                     Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
                     Region = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION"),
+                },
+                Tags = new Dictionary<string, string>
+                {
+                    { "github", "pipeline" },
+                    { "build", "teste" }
                 }
             });
             app.Synth();
